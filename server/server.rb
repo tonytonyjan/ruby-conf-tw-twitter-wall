b64ad993f83f20a860886d36aa552570e5f0c1b2
Tony Jian <tonytonyjan@gmail.com>
2014-03-17 22:59:26 +0800

    admin init

--- server/server.rb
+++ server/server.rb
@@ -4,7 +4,6 @@ require 'em-twitter'
 require 'json'
 require 'settingslogic'
 require 'settings'
-
 EM.run {
   @channel = EM::Channel.new
   # ref: https://dev.twitter.com/docs/streaming-apis/parameters#track
@@ -13,20 +12,26 @@ EM.run {
     # ref: https://dev.twitter.com/docs/platform-objects/tweets
     @channel.push(%Q({"op": "tweet", "data": #{tweet}}))
   end
-  EM::WebSocket.start(host: "0.0.0.0", port: 8080, debug: true) do |ws|
-    ws.onopen {
-      sid = @channel.subscribe { |msg| ws.send msg }
-      puts "##{sid} connected."
-      ws.send({op: :msg, data: 'Welcome!'}.to_json)
+  EM::WebSocket.run(host: "0.0.0.0", port: 8080, debug: true) do |ws|
+    ws.onopen { |handshake|
+      case handshake.path
+      when '/'
+        sid = @channel.subscribe { |msg| ws.send msg }
+        puts "##{sid} connected."
+        ws.send({op: :msg, data: 'Welcome!'}.to_json)
 
-      ws.onmessage { |msg|
-        @channel.push "<##{sid}>: #{msg}"
-      }
+        ws.onmessage { |msg|
+          @channel.push "<##{sid}>: #{msg}"
+        }
 
-      ws.onclose {
-        @channel.unsubscribe(sid)
-        puts "##{sid} closed."
-      }
+        ws.onclose {
+          @channel.unsubscribe(sid)
+          puts "##{sid} closed."
+        }
+      when '/admin'
+        ws.send({op: :msg, data: 'Welcome to control panel!'})
+        ws.onmessage{ |msg| }
+      end
     }
   end
 
