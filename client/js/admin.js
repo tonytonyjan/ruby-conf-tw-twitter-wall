a983f34c13a22253099e7ac0da4e9fbee142d476
Tony Jian <tonytonyjan@gmail.com>
2014-04-04 19:16:38 +0800

    rm admin

--- client/js/admin.js
+++
@@ -1,9 +0,0 @@
-(function start(websocketServerLocation){
-  sock = new WebSocket(websocketServerLocation);
-  sock.onmessage = function(e){
-  }
-  sock.onclose = function(e){
-    console.warn(e);
-    setTimeout(function(){start(websocketServerLocation)}, 5000);
-  };
-})('ws://localhost:8080/admin');
--- server/server.rb
+++ server/server.rb
@@ -14,24 +14,18 @@ EM.run {
   end
   EM::WebSocket.run(host: "0.0.0.0", port: 8080, debug: true) do |ws|
     ws.onopen { |handshake|
-      case handshake.path
-      when '/'
-        sid = @channel.subscribe { |msg| ws.send msg }
-        puts "##{sid} connected."
-        ws.send({op: :msg, data: 'Welcome!'}.to_json)
+      sid = @channel.subscribe { |msg| ws.send msg }
+      puts "##{sid} connected."
+      ws.send({op: :msg, data: 'Welcome!'}.to_json)
 
-        ws.onmessage { |msg|
-          @channel.push "<##{sid}>: #{msg}"
-        }
+      ws.onmessage { |msg|
+        @channel.push "<##{sid}>: #{msg}"
+      }
 
-        ws.onclose {
-          @channel.unsubscribe(sid)
-          puts "##{sid} closed."
-        }
-      when '/admin'
-        ws.send({op: :msg, data: 'Welcome to control panel!'})
-        ws.onmessage{ |msg| }
-      end
+      ws.onclose {
+        @channel.unsubscribe(sid)
+        puts "##{sid} closed."
+      }
     }
   end
 
